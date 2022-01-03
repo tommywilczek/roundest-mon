f568f25884ea2e9d7a8006e4e43ede695488aa51
the0xcollective <tommywilczek@utexas.edu>
2022-01-03 15:20:29 -0600

    Ad vote buttons and fix tRPC url gen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { withTRPC } from '@trpc/next';
+import { AppRouter } from '@/backend/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-import { withTRPC } from '@trpc/next';
-import { AppType } from 'next/dist/shared/lib/utils';
-import { AppRouter } from '@/backend/router';
+function getBaseUrl() {
+  if (process.browser) return ""; // Browser should use current path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+}
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
@@ -16,9 +21,11 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+    const url = `${getBaseUrl()}/api/trpc`;
+    // const url = process.env.VERCEL_URL
+    //   ? `https://${process.env.VERCEL_URL}/api/trpc`
+    //   : 'http://localhost:3000/api/trpc';
+    // ^ this happened in minute 53 of the video
 
     return {
       url,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,27 @@ import { trpc } from '@/utils/trpc'
 import { useState } from 'react';
 import 'tailwindcss/tailwind.css'
 
+const btn =
+  "inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+
 export default function Home() {
 
   const [ids, updateIds] = useState(getOptionsForVote());
 
-  const [first, second] = ids;
+  const [firstId, secondId] = ids;
 
-  const firstPokemon = trpc.useQuery(['get-pokemon-by-id', { id: first }])
-  const secondPokemon = trpc.useQuery(['get-pokemon-by-id', { id: second }])
+  const firstPokemon = trpc.useQuery(['get-pokemon-by-id', { id: firstId }])
+  const secondPokemon = trpc.useQuery(['get-pokemon-by-id', { id: secondId }])
   console.log('firstPokemon :>> ', firstPokemon.data);
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) {
     return null;
+  }
+
+  const voteForRoundest = (selected: number) => {
+    console.log('selected :>> ', selected);
+    // todo: fire mutation to persist changes
+    updateIds(getOptionsForVote());
   }
 
   return (
@@ -22,22 +31,24 @@ export default function Home() {
       <div className='text-2xl text-center'>Which Pokemon is rounder???</div>
       <div className='p-2'></div>
       <div className='border rounded p-8 flex items-center justify-between max-w-2xl'>
-        <div className='w-64 h-64 flex flex-col'>
+        <div className='w-64 h-64 flex flex-col items-center'>
           <img
             src={firstPokemon.data?.sprite ?? ''}
             alt={firstPokemon.data?.name}
             className='w-full'
           />
           <div className='text-xl text-center capitalize mt-[-2rem]'>{firstPokemon.data?.name}</div>
+          <button className={btn} onClick={() => voteForRoundest(firstId)}>Rounder</button>
         </div>
         <div className='p-8'>Vs</div>
-        <div className='w-64 h-64 flex flex-col'>
+        <div className='w-64 h-64 flex flex-col items-center'>
           <img
             src={secondPokemon.data?.sprite ?? ''}
             alt={secondPokemon.data?.name}
             className='w-full'
           />
           <div className='text-xl text-center capitalize mt-[-2rem]'>{secondPokemon.data?.name}</div>
+          <button className={btn} onClick={() => voteForRoundest(secondId)}>Rounder</button>
         </div>
         <div className='p-2'></div>
       </div>
